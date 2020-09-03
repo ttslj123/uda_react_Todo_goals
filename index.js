fe815537c8645c5ab5e3b31961fa3581dab592c1
ttslj123 <zhoumengsucn@gmail.com>
2020-09-03 13:37:19 -0700

    pure function

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
+function todos (state, action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
 function createStore() {
     // 1. The state
     // 2. Get the state
     // 3. Listen to changes on the state
     // 4. Update the state
     let state
+    let listeners = []
 
     const getState = () => state
 
+    const subscribe = () => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l!==listener)
+        }
+    }
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
-
-const store = createStore()
-store.subscribe(() => {
-    console.log('The new state is :' , store.getState)
-})
